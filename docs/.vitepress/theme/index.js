519d2d196925937a53d993c2d60354136a492c54
mangho <jaydon007@163.com>
2024-05-30 15:57:37 +0800

    删除main分支

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import CusomFooter from './custom-footer.vue'
 import './custom.css'
+
 export default {
   extends: DefaultTheme,
   // 自定义footer
