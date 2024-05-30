794281ab9c10c9cae0c17b139d4a42bc3f6a6487
mangho <jaydon007@163.com>
2024-05-30 15:36:09 +0800

    test deploy

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,7 +2,6 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import CusomFooter from './custom-footer.vue'
 import './custom.css'
-
 export default {
   extends: DefaultTheme,
   // 自定义footer
